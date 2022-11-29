b133824feb4d1cbf2b016744d3d65025e0f6de60
Zuzana Burešová <zuzana.buresova24@gmail.com>
2022-11-29 07:05:38 +0100

    abcd

--- public/config.php
+++ public/config.php
@@ -1,9 +1,11 @@
 <?php
+define('DB_SERVER', 'containers-us-west-56.railway.app');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'YUFufvjA6WN8yP3xV8KO');
+define('DB_NAME', 'railway');
+define('DB_PORT', 6035);
 /* Attempt to connect to MySQL database */
-$link = mysqli_init();
-if (!mysqli_real_connect($link, 'localhost', 'xbudai01', '3moparge', 'xbudai01', 0, '/var/run/mysql/mysql.sock')) {
-	die('cannot connect '.mysqli_connect_error());
-} 
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
